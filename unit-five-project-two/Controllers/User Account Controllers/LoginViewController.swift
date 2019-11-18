79d53e94663135dc53e7f092c3ae0290475432cd
levi davis <levidavis@pursuit.org>
2019-11-18 17:36:56 -0500

    Removes google plist

--- unit-five-project-two/Controllers/User Account Controllers/LoginViewController.swift
+++ unit-five-project-two/Controllers/User Account Controllers/LoginViewController.swift
@@ -9,6 +9,69 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    
+    //MARK: - UI Objects
+    
+    lazy var logoLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = "Pursuitstagram"
+        label.font = UIFont(name: "Verdana-Bold", size: 60)
+        label.textColor = UIColor(red: 255/255, green: 86/255, blue: 0/255, alpha: 1.0)
+        label.backgroundColor = .clear
+        label.textAlignment = .center
+        return label
+    }()
+    
+    lazy var emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter Email"
+        textField.font = UIFont(name: "Verdana", size: 14)
+        textField.backgroundColor = .white
+        textField.borderStyle = .bezel
+        textField.autocorrectionType = .no
+        textField.addTarget(self, action: #selector(validateFields), for: .editingChanged)
+        return textField
+    }()
+    
+    lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter Password"
+        textField.font = UIFont(name: "Verdana", size: 14)
+        textField.backgroundColor = .white
+        textField.borderStyle = .bezel
+        textField.autocorrectionType = .no
+        textField.isSecureTextEntry = true
+        textField.addTarget(self, action: #selector(validateFields), for: .editingChanged)
+        return textField
+    }()
+    
+    lazy var loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Login", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Verdana-Bold", size: 14)
+        button.backgroundColor = UIColor(red: 255/255, green: 67/255, blue: 0/255, alpha: 1)
+        button.layer.cornerRadius = 5
+        button.addTarget(self, action: #selector(tryLogin), for: .touchUpInside)
+        button.isEnabled = false
+        return button
+    }()
+    
+    lazy var createAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        let attributedTitle = NSMutableAttributedString(string: "Dont have an account?  ",
+                                                        attributes: [
+                                                            NSAttributedString.Key.font: UIFont(name: "Verdana", size: 14)!,
+                                                            NSAttributedString.Key.foregroundColor: UIColor.white])
+        attributedTitle.append(NSAttributedString(string: "Sign Up",
+                                                  attributes: [NSAttributedString.Key.font: UIFont(name: "Verdana-Bold", size: 14)!,
+                                                               NSAttributedString.Key.foregroundColor:  UIColor(red: 17/255, green: 154/255, blue: 237/255, alpha: 1)]))
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        button.addTarget(self, action: #selector(showSignUp), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
