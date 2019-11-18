ed4b0c4a47aacfb4f2570b8fcf4acbef21b3d413
levi davis <levidavis@pursuit.org>
2019-11-18 16:37:50 -0500

    Adds AppUser functions to FirestoreService

--- unit-five-project-two/Utilities/Firebase/FirestoreService.swift
+++ unit-five-project-two/Utilities/Firebase/FirestoreService.swift
@@ -22,81 +22,72 @@ enum SortingCriteria: String {
             return true
         }
     }
+}
     
-    class FirestoreService {
-        
-        static let manager = FirestoreService()
-        
-        private let db = Firestore.firestore()
-        
-//        MARK: - AppUsers
-        
-//        func createAppUser(user: AppUser)
+class FirestoreService {
+    
+    static let manager = FirestoreService()
+    
+    private let db = Firestore.firestore()
+    
+    //        MARK: - AppUsers
+    
+    func createAppUser(user: AppUser, completion: @escaping (Result<(), Error>) -> ()) {
+        var fields: [String : Any] = user.fieldsDict
+        fields["dateCreated"] = Date()
+        db.collection(FireStoreCollections.users.rawValue).document(user.uid).setData(fields) { (error) in
+            if let error = error {
+                completion(.failure(error))
+            }
+            completion(.success(<#T##()#>))
+        }
     }
+    
+    func updateCurrentUser(userName: String? = nil, photoURL: URL? = nil, completion: @escaping (Result <(), Error>) -> ()) {
+        guard let userID = FirebaseAuthService.manager.currentUser?.uid else {
+            return
+        }
+        
+        var updateFields = [String : Any]()
+        if let user = userName {
+            updateFields["userName"] = user
+        }
+        
+        if let photo = photoURL {
+            updateFields["photoURL"] = photo.absoluteString
+        }
+        
+//        PUT request
+        db.collection(FireStoreCollections.users.rawValue).document(userID).updateData(updateFields) { (error) in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(<#T##()#>))
+            }
+        }
+    }
+    
+    func getAllUsers(completion: @escaping (Result<[AppUser], Error>) -> ()) {
+        db.collection(FireStoreCollections.users.rawValue).getDocuments { (snapshot, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                let users = snapshot?.documents.compactMap({ (snapshot) -> AppUser? in
+                    let userID = snapshot.documentID
+                    let user = AppUser(from: snapshot.data(), id: userID)
+                    return user
+                })
+                completion(.success(users ?? []))
+            }
+        }
+    }
+//    MARK: - Posts
+    func createPost(){}
+    
 }
 
-/*
 
- class FirestoreService {
-     static let manager = FirestoreService()
-     
-     private let db = Firestore.firestore()
-     
-     //MARK: AppUsers
-     func createAppUser(user: AppUser, completion: @escaping (Result<(), Error>) -> ()) {
-         var fields = user.fieldsDict
-         fields["dateCreated"] = Date()
-         db.collection(FireStoreCollections.users.rawValue).document(user.uid).setData(fields) { (error) in
-             if let error = error {
-                 completion(.failure(error))
-                 print(error)
-             }
-             completion(.success(()))
-         }
-     }
-     
-     func updateCurrentUser(userName: String? = nil, photoURL: URL? = nil, completion: @escaping (Result<(), Error>) -> ()){
-         guard let userId = FirebaseAuthService.manager.currentUser?.uid else {
-             //MARK: TODO - handle can't get current user
-             return
-         }
-         var updateFields = [String:Any]()
-         
-         if let user = userName {
-             updateFields["userName"] = user
-         }
-         
-         if let photo = photoURL {
-             updateFields["photoURL"] = photo.absoluteString
-         }
-         
-        
-         //PUT request
-         db.collection(FireStoreCollections.users.rawValue).document(userId).updateData(updateFields) { (error) in
-             if let error = error {
-                 completion(.failure(error))
-             } else {
-                 completion(.success(()))
-             }
-             
-         }
-     }
-     
-     func getAllUsers(completion: @escaping (Result<[AppUser], Error>) -> ()) {
-         db.collection(FireStoreCollections.users.rawValue).getDocuments { (snapshot, error) in
-             if let error = error {
-                 completion(.failure(error))
-             } else {
-                 let users = snapshot?.documents.compactMap({ (snapshot) -> AppUser? in
-                     let userID = snapshot.documentID
-                     let user = AppUser(from: snapshot.data(), id: userID)
-                     return user
-                 })
-                 completion(.success(users ?? []))
-             }
-         }
-     }
-     
+/**
      //MARK: Posts
      func createPost(post: Post, completion: @escaping (Result<(), Error>) -> ()) {
          var fields = post.fieldsDict
