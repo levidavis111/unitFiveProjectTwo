e4000787ef1237065679ecd1b0f665ef836344d2
levi davis <levidavis@pursuit.org>
2019-11-18 15:49:44 -0500

    Adds FirebaseAuthService class

--- unit-five-project-two/Utilities/Firebase/FirebaseAuthService.swift
+++ unit-five-project-two/Utilities/Firebase/FirebaseAuthService.swift
@@ -50,40 +50,18 @@ class FirebaseAuthService {
         })
     }
     
+    func loginUser(email: String, password: String, completion: @escaping(Result<(), Error>) -> ()) {
+        auth.signIn(withEmail: email, password: password) { (result, error) in
+//            if let user = result?.user {}
+            
+            if result?.user != nil {
+                completion(.success(<#T##()#>))
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    private init() {}
+    
 }
-
-
-
-/*
-     
-     func updateUserFields(userName: String? = nil,photoURL: URL? = nil, completion: @escaping (Result<(),Error>) -> ()){
-         let changeRequest = auth.currentUser?.createProfileChangeRequest()
-         if let userName = userName {
-             changeRequest?.displayName = userName
-         }
-         if let photoURL = photoURL {
-             changeRequest?.photoURL = photoURL
-         }
-         changeRequest?.commitChanges(completion: { (error) in
-             if let error = error {
-                 completion(.failure(error))
-             } else {
-                 completion(.success(()))
-             }
-         })
-     }
-     
-     func loginUser(email: String, password: String, completion: @escaping (Result<(), Error>) -> ()) {
-         auth.signIn(withEmail: email, password: password) { (result, error) in
-             if let user = result?.user {
-                 completion(.success(()))
-             } else if let error = error {
-                 completion(.failure(error))
-             }
-         }
-     }
-
-     private init () {}
- }
-
- */
